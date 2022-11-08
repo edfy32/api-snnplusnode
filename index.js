c97eef7a74589890e9bab770c51ae17551be14c7
victor <victorhugosouza.hs@gmail.com>
2022-11-08 00:56:10 -0300

    ajuste1

--- index.js
+++ index.js
@@ -341,9 +341,8 @@ const express = require('express');
   })
 
 
-  const port = process.env.PORT || 8080;
-  app.listen(port, () => {
-    console.log("Server listening on port " + port);
+  app.listen(process.env.PORT || 3000, function(){
+    console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
   });
 
 })();
